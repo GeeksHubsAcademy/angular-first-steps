b1fcf56f5bbf75c064bef13c0f075232646e6d5b
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-03-24 13:34:59 +0100

    added routes both for contact and home components and also added router links to the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './containers/contact/contact.component';
+import { HomeComponent } from './containers/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './containers/contact/contact.component';
+import { HomeComponent } from './containers/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
